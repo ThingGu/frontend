2d2629351860bfe8bef591be1ae4fe9ea6441057
saehee-park <hee00315@gmail.com>
2021-08-25 05:14:08 +0900

    밥/술띵구 첫화면

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import MainOption from './MainOption';
+import BabTinggu from './babThinggu';
+import AlcholTinggu from './AlcholTinggu';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class extends React.Component{
+  render() {
+    return <MainOption />;
+    // return <BabTinggu />;
+    // return <AlcholTinggu />;
+    
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
