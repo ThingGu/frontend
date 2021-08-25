2581f296614bcd79cfa5e321ee0b93f9b4142bb7
saehee-park <hee00315@gmail.com>
2021-08-26 00:23:35 +0900

    술 띵구 탐색

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import MainOption from './MainOption';
 import BabTinggu from './babThinggu';
 import AlcholTinggu from './AlcholTinggu';
 import BabCard from './BabCard';
+import DrinkCard from './DrinkCard';
 
 export default class extends React.Component{
   render() {
     // return <MainOption />;
     // return <BabTinggu />;
     // return <AlcholTinggu />;
-    return <BabCard />;
+    // return <BabCard />;
+    return <DrinkCard/>;
   }
 }
