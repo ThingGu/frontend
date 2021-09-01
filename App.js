68b3e6cbbe805152cf50bc02e123009aefb92596
saehee-park <hee00315@gmail.com>
2021-09-01 16:48:57 +0900

    하단 메뉴바 수정

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MainOption from './MainOption';
 import BabCard from './BabCard';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+const Tab = createBottomTabNavigator();
 
 function HomeScreen() {
   return <MainOption />;
@@ -12,14 +14,38 @@ function AlarmScreen(){
   return <BabCard />;
 }
 
-const Tab = createBottomTabNavigator();
-
 function MyTabs() {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="name" component={HomeScreen} />
-      <Tab.Screen name="Alarm" component={AlarmScreen} />
-    </Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === '홈') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === '알림'){
+              iconName = focused ? 'bell' : 'bell-outline';
+            } else if (route.name === '채팅'){
+              iconName = focused ? 'message-processing' : 'message-processing-outline';
+            }
+            else if (route.name === '프로필'){
+              iconName = focused ? 'account-circle' : 'account-circle-outline';
+            }
+            // You can return any component that you like here!
+            return <Icon name={iconName} size={size}  color={color}/>;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'black',
+          inactiveTintColor: 'gray'
+        }}
+      >
+        <Tab.Screen name="홈" component={HomeScreen} />
+        <Tab.Screen name="알림" component={AlarmScreen} />
+        <Tab.Screen name="채팅" component={HomeScreen} />
+        <Tab.Screen name="프로필" component={AlarmScreen} />
+      </Tab.Navigator>
   );
 }
 
